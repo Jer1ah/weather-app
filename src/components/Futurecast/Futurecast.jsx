815c4494b56a880a16e9ffe9d5bdd1cf53e477c0
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-10-23 10:55:32 -0400

    adding to design

--- src/components/Futurecast/Futurecast.jsx
+++ src/components/Futurecast/Futurecast.jsx
@@ -1,86 +1,88 @@
 import React from 'react';
 import styles from './Futurecast.module.css';
 
+import image from '../../images/cloudy.svg';
+
 const Futurecast = () => {
     return (
         <div className={styles.futurecast}>
             <ul className={styles.list}>
                 <li className={styles.listItem}>
                     <h4>Thursday</h4>
-                    <img src="#" alt="Weather Icon"/>
-                    <div className={styles.placeholder}>
+                    <img src={image} alt="Weather Icon"/>
+                    <div className={styles.highTemp}>
                         <span>High</span>
                         <h5>79&deg;</h5>
                     </div>
-                    <div className="lowTemp">
+                    <div className={styles.lowTemp}>
                         <span>Low</span>
                         <h5>58&deg;</h5>
                     </div>
-                    <div className="rainChance">
+                    <div className={styles.rainChance}>
                         <span>Rain</span>
                         <h5>27%</h5>
                     </div>
                 </li>
                 <li className={styles.listItem}>
                 <h4>Friday</h4>
-                    <img src="#" alt="Weather Icon"/>
-                    <div className="highTemp">
+                    <img src={image} alt="Weather Icon"/>
+                    <div className={styles.highTemp}>
                         <span>High</span>
                         <h5>79&deg;</h5>
                     </div>
-                    <div className="lowTemp">
+                    <div className={styles.lowTemp}>
                         <span>Low</span>
                         <h5>58&deg;</h5>
                     </div>
-                    <div className="rainChance">
+                    <div className={styles.rainChance}>
                         <span>Rain</span>
                         <h5>27%</h5>
                     </div>
                 </li>
                 <li className={styles.listItem}>
                 <h4>Saturday</h4>
-                    <img src="#" alt="Weather Icon"/>
-                    <div className="highTemp">
+                    <img src={image} alt="Weather Icon"/>
+                    <div className={styles.highTemp}>
                         <span>High</span>
                         <h5>79&deg;</h5>
                     </div>
-                    <div className="lowTemp">
+                    <div className={styles.lowTemp}>
                         <span>Low</span>
                         <h5>58&deg;</h5>
                     </div>
-                    <div className="rainChance">
+                    <div className={styles.rainChance}>
                         <span>Rain</span>
                         <h5>27%</h5>
                     </div>
                 </li>
                 <li className={styles.listItem}>
                 <h4>Sunday</h4>
-                    <img src="#" alt="Weather Icon"/>
-                    <div className="highTemp">
+                    <img src={image} alt="Weather Icon"/>
+                    <div className={styles.highTemp}>
                         <span>High</span>
                         <h5>79&deg;</h5>
                     </div>
-                    <div className="lowTemp">
+                    <div className={styles.lowTemp}>
                         <span>Low</span>
                         <h5>58&deg;</h5>
                     </div>
-                    <div className="rainChance">
+                    <div className={styles.rainChance}>
                         <span>Rain</span>
                         <h5>27%</h5>
                     </div>
                 </li>
                 <li className={styles.listItem}>
                 <h4>Monday</h4>
-                    <img src="#" alt="Weather Icon"/>
-                    <div className="highTemp">
+                    <img src={image} alt="Weather Icon"/>
+                    <div className={styles.highTemp}>
                         <span>High</span>
                         <h5>79&deg;</h5>
                     </div>
-                    <div className="lowTemp">
+                    <div className={styles.lowTemp}>
                         <span>Low</span>
                         <h5>58&deg;</h5>
                     </div>
-                    <div className="rainChance">
+                    <div className={styles.rainChance}>
                         <span>Rain</span>
                         <h5>27%</h5>
                     </div>
--- src/components/Hourly/Hourly.jsx
+++ src/components/Hourly/Hourly.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import styles from './Hourly.module.css';
 
+import image from '../../images/cloudy.svg';
+
 const Hourly = () => {
     return (
         <div className={styles.hourly}>
             <ul className={styles.list}>
                 <li className={styles.listItem}>
                     <h5>3pm</h5>
-                    <img src="#" alt="Weather Icon"/>
+                    <img src={image} alt="Weather Icon"/>
                     <span>39&deg;</span>
                 </li>
                 <li className={styles.listItem}>
                 <h5>4pm</h5>
-                    <img src="#" alt="Weather Icon"/>
+                    <img src={image} alt="Weather Icon"/>
                     <span>40&deg;</span>
                 </li>
                 <li className={styles.listItem}>
                 <h5>5pm</h5>
-                    <img src="#" alt="Weather Icon"/>
+                    <img src={image} alt="Weather Icon"/>
                     <span>38&deg;</span>
                 </li>
             </ul>
