c1a934545c71c835c40323fd1b4784e670251c16
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-11-07 19:05:07 -0500

    Updates

---
+++ precache-manifest.34dcf4b3eba3b2ffd284b4d9df6e130c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab38fb0348600d37513e3dd30721760",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "2bde782dcdb98c10a165",
+    "url": "/weather-app/static/css/main.d538b7e6.chunk.css"
+  },
+  {
+    "revision": "62d5ad7b13c5e4108835",
+    "url": "/weather-app/static/js/2.95514ff9.chunk.js"
+  },
+  {
+    "revision": "7c514825c1a9c4a61543a783549fbf2b",
+    "url": "/weather-app/static/js/2.95514ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bde782dcdb98c10a165",
+    "url": "/weather-app/static/js/main.1026b830.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "bfd87d49d8ecc61be52aaac75c7dc289",
+    "url": "/weather-app/static/media/cloudy.bfd87d49.svg"
+  },
+  {
+    "revision": "98a800c978c0960e8b9113411ca93325",
+    "url": "/weather-app/static/media/moon.98a800c9.svg"
+  },
+  {
+    "revision": "2029286ba1a0a2280bc867bbc671e80c",
+    "url": "/weather-app/static/media/partly-cloudy-night.2029286b.svg"
+  },
+  {
+    "revision": "bdfd90660e255931a9574cc6c4371fbd",
+    "url": "/weather-app/static/media/partly-cloudy.bdfd9066.svg"
+  },
+  {
+    "revision": "5fcdec4dadf12c39bfdf409a2e8391e3",
+    "url": "/weather-app/static/media/rain.5fcdec4d.svg"
+  },
+  {
+    "revision": "a88d50f6b4b8dfa6111bea5077bc9967",
+    "url": "/weather-app/static/media/snow.a88d50f6.svg"
+  },
+  {
+    "revision": "e1bb2cc73021b3967272fca53a84621f",
+    "url": "/weather-app/static/media/sun.e1bb2cc7.svg"
+  },
+  {
+    "revision": "381339ecf9ca80876e4d0756ce80462d",
+    "url": "/weather-app/static/media/thunderstorm.381339ec.svg"
+  }
+]);
